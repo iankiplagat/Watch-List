3bd69761786dfdcb312095ca08f89babfec96634
Kasparov <ianjkiplagat@gmail.com>
2021-04-23 07:42:06 +0300

    add .gitignore

--- start.sh
+++
@@ -1,7 +0,0 @@
-export MOVIE_API_KEY='377be23ac3905559d2474df3b43bf702'
-export SECRET_KEY='31df13c266b16349be2df354665eb76e'
-
-export MAIL_USERNAME=ianjkiplagat@gmail.com
-export MAIL_PASSWORD=kip@19@25
-
-python3.8 manage.py server
